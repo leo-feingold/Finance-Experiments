4c8fb6c8b472ebf394ea1c543c4bc61811482376
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-27 23:13:39 -0400

    commit

--- Moving Average/criticalPointMovingAverage.py
+++ Moving Average/criticalPointMovingAverage.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-stock = 'IBM'
+show_moving_averages = False
+stock = 'AMZN'
 start = '2015-01-01'
 stop = '2024-05-16'
 
@@ -39,8 +40,9 @@ def visualizeData(df, crossovers, crossover_others):
     fig, axs = plt.subplots(1,1, figsize=(10, 7.5))
 
     axs.plot(df.index, df.Close, label="Stock Price", color="red", zorder=1)
-    #axs.plot(df.index, df["50DMA"], label="50DMA", color="blue", linestyle='--', zorder=2)
-    #axs.plot(df.index, df["200DMA"], label="200DMA", color="green", linestyle='--', zorder=3)
+    if show_moving_averages:
+        axs.plot(df.index, df["50DMA"], label="50DMA", color="blue", linestyle='--', zorder=2)
+        axs.plot(df.index, df["200DMA"], label="200DMA", color="green", linestyle='--', zorder=3)
     axs.scatter(crossovers, df.loc[crossovers, "Close"], label="Golden Cross", color="gold", zorder=4)
     axs.scatter(crossover_others, df.loc[crossover_others, "Close"], label="Death Cross", color='#5e041a', zorder=5)
     axs.set_title("Stock Price Over Time")
