f20a169e4bfbfdef98504bf8332d8cddb9a379f0
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-27 22:10:08 -0400

    commit

---
+++ Moving Average/criticalPointMovingAverage.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+stock = 'IBM'
+start = '2015-01-01'
+stop = '2024-05-16'
+
+def main():
+    print("hello")
+    
+if __name__ == "__main__":
+    main()
